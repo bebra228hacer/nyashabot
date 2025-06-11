ceb37393027c90c2790389c02b9fa562a4a922f2
bebra228hacer <ooo.dima.molobech@gmail.com>
2025-06-11 13:10:38 +0300

    Основная обвязка готова

--- main.py
+++ main.py
@@ -1,12 +1,19 @@
 import aiosqlite
 import json
 
+import user_db
+from user_db import User
+
+
 import os
 import asyncio
 from dotenv import load_dotenv
 from aiogram import Bot, Dispatcher, types, F
 from aiogram.filters.command import Command
+from aiogram.filters import Filter
 from aiogram.utils.keyboard import ReplyKeyboardBuilder
+
+from openrouters import chat_stream, send_request_to_openrouter
 
 with open("messages.json", encoding='utf-8') as ofile:
     MESSAGES = json.load(ofile)
@@ -17,77 +24,93 @@ DEBUG = bool(int(os.environ.get("DEBUG")))
 DEBUG_CHAT = int(os.environ.get("DEBUG_CHAT"))
 DATABASE_NAME = os.environ.get("DATABASE_NAME")
 TABLE_NAME =  os.environ.get("TABLE_NAME")
+with open("prompts.json", encoding='utf-8') as ofile:
+    DEFAULT_PROMPT = json.load(ofile)
+    DEFAULT_PROMPT = DEFAULT_PROMPT['DEFAULT_PROMPT']
+
 
 bot = Bot(token = TG_TOKEN)
 dp = Dispatcher()
 
-async def check_db():
-    async with aiosqlite.connect(DATABASE_NAME) as db:
-        async with db.cursor() as cursor:
-            await cursor.execute(
-                f"""
-                CREATE TABLE IF NOT EXISTS {TABLE_NAME} (
-                    id INTEGER PRIMARY KEY,  --  ID is now the PRIMARY KEY and NOT AUTOINCREMENT
-                    name TEXT,
-                    sub_lvl INTEGER,
-                    sub_period INTEGER,
-                    is_admin INTEGER
-                )
-                """
-            )
-        await db.commit()
-        return "Бд подгружена успешно"
+
 
 async def f_debug(message_chat_id, message_id):
     if DEBUG:
         await bot.forward_message(chat_id=DEBUG_CHAT, from_chat_id = message_chat_id, message_id=message_id)
-    
 
+
+class UserNotInDB(Filter):
+    async def __call__(self, message: types.Message) -> bool:
+        user_id = message.chat.id
+        return not await user_db.user_exists(user_id)
+
+class UserHaveSubLevel(Filter):
+    def __init__(self, required_sub_lvl: int):
+        self.required_sub_lvl = required_sub_lvl
+
+    async def __call__(self, message: types.Message) -> bool:
+        user = User(message.chat.id)  
+        await user.get_from_db()
+        if user:
+            return user.sub_lvl >= self.required_sub_lvl 
+        else:
+            return False  
+
+
+
+@dp.message(F.chat.id == DEBUG_CHAT)
+async def test(message):
+    #sent_msg = await message.answer("ГОВОРЯЩАЯ АДМИНИСТРАЦИЯ")
+    pass
+
+@dp.message(UserNotInDB())
+async def test(message):
+    user = message.from_user
+    if user and user.username:
+        username = user.username
+    user = User(int(message.chat.id), username)
+    await user.save_for_db()
+    builder = ReplyKeyboardBuilder()
+    builder.button(text=MESSAGES["btn_main_menu_sub"])
+    sent_msg = await message.answer(MESSAGES["msg_start"], reply_markup=builder.as_markup())
+    await f_debug(message.chat.id, message.message_id)
+    await f_debug(message.chat.id, sent_msg.message_id)
+    
 @dp.message(Command("start"))
 async def cmd_answer(message: types.Message):
     builder = ReplyKeyboardBuilder()
     builder.button(text=MESSAGES["btn_main_menu_sub"])
-    builder.button(text=MESSAGES["btn_main_menu_profile"])
+    if UserHaveSubLevel(1):
+        builder.button(text=MESSAGES["btn_main_menu_profile"])
     sent_msg = await message.answer(MESSAGES["msg_start"], reply_markup=builder.as_markup())
     await f_debug(message.chat.id, message.message_id)
     await f_debug(message.chat.id, sent_msg.message_id)
 
-@dp.message()
-async def cmd_answer(message: types.Message):
-    builder = ReplyKeyboardBuilder()
-    builder.button(text=MESSAGES["btn_main_menu_sub"])
-    builder.button(text=MESSAGES["btn_main_menu_profile"])
-    sent_msg = await message.answer(MESSAGES["msg_start"], reply_markup=builder.as_markup())
-    await f_debug(message.chat.id, message.message_id)
-    await f_debug(message.chat.id, sent_msg.message_id)
-
-
-
-@dp.message(F.text.lower())
-async def main_sub_handler(message: types.Message):
-    builder = ReplyKeyboardBuilder()
-    builder.button(text="гойда", callback_data="Nothing")
-    await message.answer("Some text here", reply_markup=builder.as_markup())
-@dp.message(F.text.lower() == "анкета")
+@dp.message(F.text.lower() == "анкета" and UserHaveSubLevel(1))
 async def main_profile_handler(message: types.Message):
-    await message.answer()
-
+    await message.answer("мяу")
 
 @dp.message()
-async def echo_handler(message: types.Message):
-    builder = ReplyKeyboardBuilder()
-    builder.button(text="гойда", callback_data="Nothing")
-    await message.answer("Some text here", reply_markup=builder.as_markup())
-@dp.message(F.text.lower() == "анкета")
-async def echo_handler(message: types.Message):
-    await message.answer()
+async def LLC_request(message: types.Message):
+    user_db = User(message.chat.id)
+    await user_db.get_from_db()
+    prompt_for_request = user_db.prompt.copy()
+    prompt_for_request.insert(0, {"role": "user", "content": DEFAULT_PROMPT})
+    print(prompt_for_request)
+    sent_msg = await message.answer(send_request_to_openrouter(prompt_for_request))
+    await user_db.update_prompt("user", message.text)
+
+    await user_db.update_prompt("assistant", sent_msg.text)
+    await user_db.update_in_db()
+    await f_debug(message.chat.id, message.message_id)
+    await f_debug(message.chat.id, sent_msg.message_id)
 
 
 async def main():
-    """Основная функция, которая запускает проверку БД и другие задачи."""
-    db_status = await check_db()
-    print(db_status) 
-    print("Start success")
+    print(await user_db.check_db()) 
+    print("Основная часть запущена")
+    print("Отладка:")
+    print()
     await dp.start_polling(bot)
 
 if __name__ == "__main__":
--- user_db.py
+++ user_db.py
@@ -2,37 +2,39 @@ import aiosqlite
 import os
 import asyncio
 from dotenv import load_dotenv
+import json
 
 load_dotenv()
 LLM_TOKEN = os.environ.get("LLM_TOKEN")
 DEBUG = bool(os.environ.get("DEBUG"))
 DEBUG_CHAT = int(os.environ.get("DEBUG_CHAT"))
 DATABASE_NAME = os.environ.get("DATABASE_NAME")
-TABLE_NAME =  os.environ.get("TABLE_NAME")
+TABLE_NAME = os.environ.get("TABLE_NAME")
+MAX_CONTEXT = os.environ.get("MAX_CONTEXT")
+
 
 class User:
-    def __init__(self, id, name=None, sub_lvl=0, sub_period=-1, is_admin=0):
+    def __init__(
+        self,
+        id,
+        name=None,
+        prompt=[],
+        sub_lvl=0,
+        sub_id=0,
+        sub_period=-1,
+        is_admin=0,
+    ):
         self.id = id
         self.name = name
+        self.prompt = prompt
         self.sub_lvl = sub_lvl
+        self.sub_id = sub_id
         self.sub_period = sub_period
         self.is_admin = is_admin
 
     def __repr__(self):
-        return f"User(id={self.id}, name='{self.name}', sub_lvl={self.sub_lvl}, sub_period={self.sub_period}, is_admin={self.is_admin})"
+        return f"User(id={self.id}, \n name={self.name}, \n prompt={self.prompt}, \n sub_lvl={self.sub_lvl}, \n sub_id={self.sub_id}, \n sub_period={self.sub_period}, \n is_admin={self.is_admin})"
 
-    async def save_for_db(self):
-        async with aiosqlite.connect(DATABASE_NAME) as db:
-            cursor = await db.cursor()
-            sql_insert = f"""
-                        INSERT INTO {TABLE_NAME} (id, name, sub_lvl, sub_period, is_admin)
-                        VALUES (?, ?, ?, ?, ?)
-                    """
-            values = (self.id, self.name, self.sub_lvl, self.sub_period, self.is_admin)
-            await cursor.execute(sql_insert, values)
-            await db.commit()
-            await cursor.close()
-    
     async def get_from_db(self):
         async with aiosqlite.connect(DATABASE_NAME) as db:
             cursor = await db.cursor()
@@ -42,41 +44,121 @@ class User:
             if row:
                 self.id = row[0]
                 self.name = row[1]
-                self.sub_lvl = row[2]
-                self.sub_period = row[3]
-                self.is_admin = row[4]
-            await db.close()
-            
-    
+                self.prompt = json.loads(row[2])
+                self.sub_lvl = row[3]
+                self.sub_id = row[4]
+                self.sub_period = row[5]
+                self.is_admin = row[6]
+
+    async def __call__(self, user_id):
+        async with aiosqlite.connect(DATABASE_NAME) as db:
+            cursor = await db.cursor()
+            sql = f"SELECT * FROM {TABLE_NAME} WHERE id = ?"
+            await cursor.execute(sql, (user_id,))
+            row = await cursor.fetchone()
+            if row:
+                user = User(
+                    id=row[0],
+                    name=row[1],
+                    prompt=json.loads(row[2]),
+                    sub_lvl=row[3],
+                    sub_id=row[4],
+                    sub_period=row[5],
+                    is_admin=row[6],
+                )
+                return user
+            else:
+                return None
+
+    async def save_for_db(self):
+        async with aiosqlite.connect(DATABASE_NAME) as db:
+            cursor = await db.cursor()
+            sql_insert = f"""
+                        INSERT INTO {TABLE_NAME} (id, name, prompt, sub_lvl, sub_id, sub_period, is_admin)
+                        VALUES (?, ?, ?, ?, ?, ?, ?)
+                    """
+            values = (
+                self.id,
+                self.name,
+                json.dumps(self.prompt),
+                self.sub_lvl,
+                self.sub_id,
+                self.sub_period,
+                self.is_admin,
+            )
+            await cursor.execute(sql_insert, values)
+            await db.commit()
+            await cursor.close()
+
+    async def update_prompt(self, role, new_request):
+        new_entry = {"role": role, "content": new_request}
+        self.prompt.append(new_entry)
+        if len(self.prompt) > 15:
+            self.prompt = self.prompt[-15:]
+
     async def update_in_db(self):
         async with aiosqlite.connect(DATABASE_NAME) as db:
             cursor = await db.cursor()
             sql_query = f"""
                 UPDATE {TABLE_NAME}
-                SET name = ?, sub_lvl = ?, sub_period = ?, is_admin = ?
+                SET name = ?, prompt = ?, sub_lvl = ?, sub_id = ?, sub_period = ?, is_admin = ?
                 WHERE id = ?
             """
-            values = (self.name, self.sub_lvl, self.sub_period, self.is_admin, self.id)
-            await cursor.execute(sql_query, values) 
-            await db.commit() 
+            values = (
+                self.name,
+                json.dumps(self.prompt),
+                self.sub_lvl,
+                self.sub_id,
+                self.sub_period,
+                self.is_admin,
+                self.id,
+            )
+            await cursor.execute(sql_query, values)
+            await db.commit()
             await cursor.close()
-        await db.close()
+
+
+async def check_db():
+    async with aiosqlite.connect(DATABASE_NAME) as db:
+        async with db.cursor() as cursor:
+            await cursor.execute(
+                f"""
+                CREATE TABLE IF NOT EXISTS {TABLE_NAME} (
+                    id INTEGER PRIMARY KEY,  --  ID is now the PRIMARY KEY and NOT AUTOINCREMENT
+                    name TEXT,
+                    prompt JSON,
+                    sub_lvl INTEGER,
+                    sub_id TEXT,
+                    sub_period INTEGER,
+                    is_admin INTEGER
+                )
+                """
+            )
+        await db.commit()
+        return "Бд подгружена успешно"
+
+
+async def user_exists(user_id):
+    async with aiosqlite.connect(DATABASE_NAME) as db:
+        cursor = await db.cursor()
+        sql = f"SELECT EXISTS(SELECT 1 FROM {TABLE_NAME} WHERE id = ?)"
+        await cursor.execute(sql, (user_id,))
+        result = (await cursor.fetchone())[0]
+        await cursor.close()
+    await db.close()
+
+    return bool(result)
 
 
 async def main():
-    Test = User(id = 1)
+    Test = User(1433319219)
     await Test.get_from_db()
     print(Test)
-    Test2 = User(5)
-    await Test2.get_from_db()
-    print(Test2)
-    Test3 = User(2)
-    
-    Test3.is_admin = 1
-    Test3.name = "LOL"
-    await Test3.update_in_db()
-    print(Test3)
-    pass
+    await Test.update_prompt("user", "мяу")
+    print(Test)
+    await Test.update_in_db()
+    print(await Test(1433319219))
+
 
 if __name__ == "__main__":
     asyncio.run(main())
