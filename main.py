985eb0a14896dec496973dc52e8c27d948faf558
bebra228hacer <brawl.master2077@gmail.com>
2025-06-23 21:31:40 +0300

    Отказ от идеи сделать админку

--- main.py
+++ main.py
@@ -9,11 +9,13 @@ from aiogram import Bot, Dispatcher, types, F
 from aiogram.filters.command import Command
 from aiogram.filters import Filter
 from aiogram.enums import ParseMode
-from aiogram.utils.keyboard import ReplyKeyboardBuilder
+from aiogram.utils.keyboard import ReplyKeyboardBuilder, InlineKeyboardBuilder
 from aiogram.exceptions import TelegramBadRequest
 from openrouters import send_request_to_openrouter
-
-
+from aiogram import Bot, Dispatcher, types
+import asyncio
+from aiogram.fsm.context import FSMContext
+from aiogram.fsm.state import StatesGroup, State
 
 with open("messages.json", encoding="utf-8") as ofile:
     MESSAGES = json.load(ofile)
@@ -35,21 +37,10 @@ bot = Bot(token=TG_TOKEN)
 dp = Dispatcher()
 
 
-
-
-
-async def f_debug(message_chat_id, message_id):
-    if DEBUG:
-        await bot.forward_message(
-            chat_id=DEBUG_CHAT, from_chat_id=message_chat_id, message_id=message_id
-        )
-
-
 class UserNotInDB(Filter):
     async def __call__(self, message: types.Message) -> bool:
         user_id = message.chat.id
         return not await user_db.user_exists(user_id)
-
 
 class UserHaveSubLevel(Filter):
     def __init__(self, required_sub_lvl: int):
@@ -68,10 +59,15 @@ class UserIsAdmin(Filter):
         user = User(message.chat.id)
         await user.get_from_db()
         if user:
-            return user.sub_lvl >= self.required_sub_lvl
+            return int(user.is_admin) >= 1
         else:
             return False
 
+async def f_debug(message_chat_id, message_id):
+    if DEBUG:
+        await bot.forward_message(
+            chat_id=DEBUG_CHAT, from_chat_id=message_chat_id, message_id=message_id
+        )
 
 
 @dp.message(F.chat.id == DEBUG_CHAT)
@@ -108,20 +104,19 @@ async def cmd_answer(message: types.Message):
     await f_debug(message.chat.id, message.message_id)
     await f_debug(message.chat.id, sent_msg.message_id)
 
-
 @dp.message(F.text.lower() == "анкета" and UserHaveSubLevel(1))
 async def main_profile_handler(message: types.Message):
-    await bot.send_message(message.chat.id, "Не нужно", parse_mode=ParseMode.MARKDOWN_V2)
-
-@dp.message(F.text.lower() == "мяу")
-async def main_profile_handler(message: types.Message):
-    await bot.send_message(message.chat.id, "**мяу**", parse_mode=ParseMode.MARKDOWN_V2)
-
+    await bot.send_message(
+        message.chat.id, "Не нужно", parse_mode=ParseMode.MARKDOWN_V2
+    )
 
 
 @dp.message()
 async def LLC_request(message: types.Message):
-    generating_message = await bot.send_message(message.chat.id, "Текст генерируется...")
+    generating_message = await bot.send_message(
+        message.chat.id, "Текст генерируется..."
+    )
+    await bot.send_chat_action(chat_id=message.chat.id, action="typing")
     user = User(message.chat.id)
     await user.get_from_db()
     await user.update_prompt("user", message.text)
@@ -130,17 +125,35 @@ async def LLC_request(message: types.Message):
     llc_msg = await send_request_to_openrouter(prompt_for_request)
 
     try:
-        await bot.edit_message_text(llc_msg, chat_id=message.chat.id, message_id=generating_message.message_id, parse_mode=ParseMode.MARKDOWN_V2)
+        await bot.edit_message_text(
+            llc_msg,
+            chat_id=message.chat.id,
+            message_id=generating_message.message_id,
+            parse_mode=ParseMode.MARKDOWN_V2,
+        )
     except TelegramBadRequest as e:
         llc_msg = re.sub(r"(\*\*|\_\_|\~\~)", r"\\\g<1>", llc_msg)
         llc_msg = re.sub(r"([\[\]()>\#\+\=\-\.!\`\|\{\}])", r"\\\g<1>", llc_msg)
         try:
-            await bot.edit_message_text(llc_msg, chat_id=message.chat.id, message_id=generating_message.message_id, parse_mode=ParseMode.MARKDOWN_V2)
+            await bot.edit_message_text(
+                llc_msg,
+                chat_id=message.chat.id,
+                message_id=generating_message.message_id,
+                parse_mode=ParseMode.MARKDOWN_V2,
+            )
         except TelegramBadRequest as e:
-            await bot.edit_message_text(llc_msg, chat_id=message.chat.id, message_id=generating_message.message_id)
+            await bot.edit_message_text(
+                llc_msg,
+                chat_id=message.chat.id,
+                message_id=generating_message.message_id,
+            )
     except Exception as e:
-        print(f"An unexpected error occurred: {e}")  
-        await bot.edit_message_text("Произошла ошибка при генерации текста.", chat_id=message.chat.id, message_id=generating_message.message_id)
+        print(f"An unexpected error occurred: {e}")
+        await bot.edit_message_text(
+            "Произошла ошибка при генерации текста.",
+            chat_id=message.chat.id,
+            message_id=generating_message.message_id,
+        )
         return
 
     await user.update_prompt("assistant", generating_message.text)
@@ -159,24 +172,23 @@ async def reminder():
         llc_msg = await send_request_to_openrouter(prompt_for_request)
         try:
             sent_msg = await bot.send_message(
-            chat_id=id, text=llc_msg,
-            parse_mode=ParseMode.MARKDOWN_V2)
+                chat_id=id, text=llc_msg, parse_mode=ParseMode.MARKDOWN_V2
+            )
         except TelegramBadRequest as e:
-            llc_msg = re.sub(r"(\*\*|\_\_|\~\~)", r"\\\g<1>", llc_msg) 
+            llc_msg = re.sub(r"(\*\*|\_\_|\~\~)", r"\\\g<1>", llc_msg)
             llc_msg = re.sub(r"([\[\]()>\#\+\=\-\.!\`\|\{\}])", r"\\\g<1>", llc_msg)
             try:
                 sent_msg = await bot.send_message(
-                chat_id=id, text=llc_msg,
-                parse_mode=ParseMode.MARKDOWN_V2)
+                    chat_id=id, text=llc_msg, parse_mode=ParseMode.MARKDOWN_V2
+                )
             except TelegramBadRequest as e:
                 sent_msg = await bot.send_message(
-                chat_id=id, text=llc_msg,
-                parse_mode=ParseMode.MARKDOWN_V2)
+                    chat_id=id, text=llc_msg, parse_mode=ParseMode.MARKDOWN_V2
+                )
         await user.update_prompt("assistant", sent_msg.text)
         user.remind_of_yourself = "2077-06-15 22:03:51"
         await user.update_in_db()
         await f_debug(id, sent_msg.message_id)
-
 
 
 async def main():
